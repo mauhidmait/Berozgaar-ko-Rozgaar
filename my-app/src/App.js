3d4e4523bd1a01308ffcae555e42668d36305832
Ayaan Siddiqui <86252500+ayaan0105@users.noreply.github.com>
2023-07-18 23:27:02 +0530

    MUI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Button from '@mui/material/Button';
 
 export default function App() {
   return (
     <div>Berozgar ko rozgar
       <div>hello</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   )
 }
